4ee9cafda487ce087873e13a9cdb32146ad130ac
fergcb <fergus.bentley@gmail.com>
2022-07-27 11:35:36 +0100

    Add some example tests

---
+++ src/test/java/uk/fergcb/sakila/RunCucumberTest.java
@@ -0,0 +1,13 @@
+package uk.fergcb.sakila;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"pretty"},
+        glue = ""
+)
+public class RunCucumberTest{
+}
