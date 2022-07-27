0d81f2625ba813bd62f13a26ff1302184a2773eb
fergcb <fergus.bentley@gmail.com>
2022-07-27 10:41:45 +0100

    Add Language Entity

---
+++ src/main/java/uk/fergcb/sakila/language/LanguageRepository.java
@@ -0,0 +1,6 @@
+package uk.fergcb.sakila.language;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface LanguageRepository extends CrudRepository<Language, Integer> {
+}
