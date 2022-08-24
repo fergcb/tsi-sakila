c0a244ec39d6ac7c0c534c3b8ab1d99aa27a900b
fergcb <fergus.bentley@gmail.com>
2022-08-24 10:41:30 +0100

    Optimize imports

--- src/main/java/uk/fergcb/sakila/data/DataSourceConfig.java
+++ src/main/java/uk/fergcb/sakila/data/DataSourceConfig.java
@@ -1,7 +1,5 @@
 package uk.fergcb.sakila.data;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
