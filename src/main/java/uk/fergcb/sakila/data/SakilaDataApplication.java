17979bbd8716092c55871e7425af0fa9f0398955
fergcb <fergus.bentley@gmail.com>
2022-08-26 10:25:07 +0100

    Add forward headers bean

--- src/main/java/uk/fergcb/sakila/data/SakilaDataApplication.java
+++ src/main/java/uk/fergcb/sakila/data/SakilaDataApplication.java
@@ -2,12 +2,22 @@ package uk.fergcb.sakila.data;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.ForwardedHeaderFilter;
 
 @SpringBootApplication
 public class SakilaDataApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SakilaDataApplication.class, args);
+	}
+
+	@Bean
+	FilterRegistrationBean<ForwardedHeaderFilter> forwardedHeaderFilter() {
+		FilterRegistrationBean<ForwardedHeaderFilter> bean = new FilterRegistrationBean<>();
+		bean.setFilter(new ForwardedHeaderFilter());
+		return bean;
 	}
 
 }
